848d6732169035ef0565ce7f198ced21ff27152d
mayar <mayarsamir82@gmail.com>
2024-12-03 00:18:57 +0200

    added files

---
+++ src/App.js
@@ -0,0 +1,38 @@
+import { useRef, useState } from "react"
+import './App.css';
+
+
+
+function App() {
+  const [todos, setTodos] = useState([]);
+  const inputRef = useRef()
+  const handleAdd = () => {
+    const text = inputRef.current.value;
+    const newItem = { completed: false, text };
+    setTodos([...todos, newItem])
+    inputRef.current.value = ""
+  }
+  const itemDone = (index) => {
+    const newTodos = [...todos];
+    newTodos[index].completed = !newTodos[index].completed;
+    setTodos(newTodos);
+    return (
+      <div className="App">
+        <h1>ToDo List</h1>
+        <div className='to-do-container'>
+          <ul>
+            {todos.map(({ text , completed }, index) => {
+              return <li className = {completed? "done" : ""} onClick={() => itemDone(index)}>{text}</li>;
+            })}  
+          </ul>
+          <input ref={inputRef} type="text" placeholder="Add a new task..." />
+          <button onClick={handleAdd}>add</button>
+
+        </div>
+
+      </div>
+    );
+  }
+}
+
+export default App;
