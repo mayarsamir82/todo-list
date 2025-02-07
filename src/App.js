304909ceaf4961b90d2dbfeef7de62f00141a7d2
Mohammid-samir <developer.mohamedsamir@gmail.com>
2025-02-07 22:09:39 +0200

    nits

--- src/App.js
+++ src/App.js
@@ -34,10 +34,13 @@ function App() {
       <h1 className="todocolor">TODO LIST</h1>
       <button type="button" class="btn btn-primary">Add Task</button>
 
+
       <div class="dropdown">
         <button class="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
           All
         </button>
+
+
 
         <ul class="dropdown-menu">
           <li><a class="dropdown-item" href="#">All</a></li>
@@ -45,6 +48,8 @@ function App() {
           <li><a class="dropdown-item" href="#">Not Completed</a></li>
         </ul>
       </div>
+
+
 
       <div className='to-do-container'>
         <ul>
@@ -56,38 +61,6 @@ function App() {
           })}
         </ul>
 
-        <div className="ulContainer">
-
-          <input className="checkBox" type="checkbox" value="" />
-
-          <div className="innerDiv">
-            <input type="text"
-              class="form-control"
-              id="usr"
-              ref={inputRef}
-              placeholder="Creat a react project 5:23 Am, 01/06/2022" />
-            <p>{formattedDate}, {formattedTime}</p>
-          </div>
-
-
-          <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="currentColor" class="bi bi-pencil-fill" viewBox="0 0 16 16">
-            <path d="M12.854.146a.5.5 0 0 0-.707 0L10.5 1.793 14.207 5.5l1.647-1.646a.5.5 0 0 0 0-.708zm.646 6.061L9.793 2.5 3.293 9H3.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.207zm-7.468 7.468A.5.5 0 0 1 6 13.5V13h-.5a.5.5 0 0 1-.5-.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.5-.5V10h-.5a.5.5 0 0 1-.175-.032l-.179.178a.5.5 0 0 0-.11.168l-2 5a.5.5 0 0 0 .65.65l5-2a.5.5 0 0 0 .168-.11z" />
-          </svg>
-
-          <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" vv fill="currentColor" class="bi bi-trash3" viewBox="0 0 16 16">
-            <path d="M6.5 1h3a.5.5 0 0 1 .5.5v1H6v-1a.5.5 0 0 1 .5-.5M11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3A1.5 1.5 0 0 0 5 1.5v1H1.5a.5.5 0 0 0 0 1h.538l.853 10.66A2 2 0 0 0 4.885 16h6.23a2 2 0 0 0 1.994-1.84l.853-10.66h.538a.5.5 0 0 0 0-1zm1.958 1-.846 10.58a1 1 0 0 1-.997.92h-6.23a1 1 0 0 1-.997-.92L3.042 3.5zm-7.487 1a.5.5 0 0 1 .528.47l.5 8.5a.5.5 0 0 1-.998.06L5 5.03a.5.5 0 0 1 .47-.53Zm5.058 0a.5.5 0 0 1 .47.53l-.5 8.5a.5.5 0 1 1-.998-.06l.5-8.5a.5.5 0 0 1 .528-.47M8 4.5a.5.5 0 0 1 .5.5v8.5a.5.5 0 0 1-1 0V5a.5.5 0 0 1 .5-.5" />
-          </svg>
-
-
-
-
-
-
-
-
-        </div>
-
-
 
         <div className="ulContainer">
 
@@ -139,17 +112,33 @@ function App() {
           </svg>
 
 
-
         </div>
 
 
 
+        <div className="ulContainer">
+
+          <input className="checkBox" type="checkbox" value="" />
+
+          <div className="innerDiv">
+            <input type="text"
+              class="form-control"
+              id="usr"
+              ref={inputRef}
+              placeholder="Creat a react project 5:23 Am, 01/06/2022" />
+            <p>{formattedDate}, {formattedTime}</p>
+          </div>
 
 
+          <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="currentColor" class="bi bi-pencil-fill" viewBox="0 0 16 16">
+            <path d="M12.854.146a.5.5 0 0 0-.707 0L10.5 1.793 14.207 5.5l1.647-1.646a.5.5 0 0 0 0-.708zm.646 6.061L9.793 2.5 3.293 9H3.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.207zm-7.468 7.468A.5.5 0 0 1 6 13.5V13h-.5a.5.5 0 0 1-.5-.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.5-.5V10h-.5a.5.5 0 0 1-.175-.032l-.179.178a.5.5 0 0 0-.11.168l-2 5a.5.5 0 0 0 .65.65l5-2a.5.5 0 0 0 .168-.11z" />
+          </svg>
 
+          <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" vv fill="currentColor" class="bi bi-trash3" viewBox="0 0 16 16">
+            <path d="M6.5 1h3a.5.5 0 0 1 .5.5v1H6v-1a.5.5 0 0 1 .5-.5M11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3A1.5 1.5 0 0 0 5 1.5v1H1.5a.5.5 0 0 0 0 1h.538l.853 10.66A2 2 0 0 0 4.885 16h6.23a2 2 0 0 0 1.994-1.84l.853-10.66h.538a.5.5 0 0 0 0-1zm1.958 1-.846 10.58a1 1 0 0 1-.997.92h-6.23a1 1 0 0 1-.997-.92L3.042 3.5zm-7.487 1a.5.5 0 0 1 .528.47l.5 8.5a.5.5 0 0 1-.998.06L5 5.03a.5.5 0 0 1 .47-.53Zm5.058 0a.5.5 0 0 1 .47.53l-.5 8.5a.5.5 0 1 1-.998-.06l.5-8.5a.5.5 0 0 1 .528-.47M8 4.5a.5.5 0 0 1 .5.5v8.5a.5.5 0 0 1-1 0V5a.5.5 0 0 1 .5-.5" />
+          </svg>
 
-
-
+        </div>
 
 
 
